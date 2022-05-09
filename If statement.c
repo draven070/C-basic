3c158f5fddcc4ba45f3a3d8d13905a95b913151e
bijudahal7 <102226807+bijudahal7@users.noreply.github.com>
2022-05-09 09:15:07 +0545

    Create If statement.c

---
+++ If statement.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{ 
+    int a,b;
+    printf("Enter two numbers:");
+    scanf("%d%d",&a,&b);
+     if(a>b)
+     {
+       printf("a is greatest");
+     }
+   getch();
+}
+      
